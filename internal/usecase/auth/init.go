d420173012645784d62439733828cb0f0e963de8
Arya Yunanta <aryayun90@gmail.com>
2022-05-22 15:12:52 +0700

    Add prevent login if email is not verified

--- internal/usecase/auth/init.go
+++ internal/usecase/auth/init.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"context"
+	errorCommon "github.com/HMIF-UNSRI/srifoton-be/common/error"
 	"github.com/HMIF-UNSRI/srifoton-be/common/jwt"
 	passCommon "github.com/HMIF-UNSRI/srifoton-be/common/password"
 	userRepo "github.com/HMIF-UNSRI/srifoton-be/internal/repository/user"
@@ -22,6 +23,10 @@ func (a authUsecase) Login(ctx context.Context, email string, password string) (
 	user, err := a.userRepository.FindByEmail(ctx, email)
 	if err != nil {
 		return accessToken, err
+	}
+
+	if !user.IsEmailVerified {
+		return accessToken, errorCommon.NewNotFoundError("user not found")
 	}
 
 	if err := a.passwordManager.CheckPasswordHash(password, user.Password); err != nil {
